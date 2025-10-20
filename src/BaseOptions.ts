d7e6a934f291fd926b8add88a466bf9f0d0af4a7
mthunzi <mthunzit@bluenova.co.za>
2025-10-20 11:44:25 +0200

    Empty deploy

--- src/BaseOptions.ts
+++ src/BaseOptions.ts
@@ -5,7 +5,7 @@ const baseOptions: Highcharts.Options = {
     text: "My First Highcharts Chart in React with TypeScript"
   },
   series: [
-    { type: "line", name: "Series 1", data: [1, 3, 2, 4, 6, 5] },
+    { type: "line", name: "Series 1", data: [1, 3, 2, 4, 6, 5] }, // Fake data
     { type: "line", name: "Series 2", data: [2, 4, 1, 5, 3, 7] },
     { type: "line", name: "Series 3", data: [5, 3, 4, 2, 6, 1] },
     { type: "line", name: "Series 4", data: [5, 3, 4, 2, 6, 1] },
